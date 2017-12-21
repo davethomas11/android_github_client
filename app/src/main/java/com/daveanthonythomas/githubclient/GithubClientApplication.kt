30eba8e39b2d76cd63c115e68850cb667bdbd392
Dave Thomas <dave.thomas@trader.ca>
2017-12-21 15:38:21 -0800

    More prep work

--- app/src/main/java/com/daveanthonythomas/githubclient/GithubClientApplication.kt
+++ app/src/main/java/com/daveanthonythomas/githubclient/GithubClientApplication.kt
@@ -1,11 +1,22 @@
 package com.daveanthonythomas.githubclient
 
+import com.daveanthonythomas.githubclient.login.LoginActivity
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import dagger.android.support.AndroidSupportInjectionModule
 
 class GithubClientApplication : DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO()
+    }
+
+    @dagger.Component(modules = [
+        AndroidSupportInjectionModule::class,
+        LoginActivity.Module::class
+    ])
+    interface Component : AndroidInjector<GithubClientApplication> {
+        @dagger.Component.Builder
+        abstract class Builder : AndroidInjector.Builder<GithubClientApplication>()
     }
 }
